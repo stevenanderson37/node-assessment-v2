cae3ff12e9201c0674bc8d692384b4d99e7e5e87
Jeremy R <jeremy@devmounta.in>
2016-08-10 15:19:50 -0600

    card accounts

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * The file must be called server.js
 * There is a package.json included with dependencies in it, you should know what to do.
 * It must use express, and body parser (Set up the app and use the body parser)
-* You will want to require the included `users.json file`  `var users = require('./users.json');`
+* You will want to require the included `accounts.json` file  `var accounts = require('./accounts.json');`
 * At the bottom of the file you must `module.exports` your express app.
 * Listen on port 3000.
 
@@ -13,47 +13,44 @@
 ## Setup your endpoints
 Create the following endpoints in `server.js` using express.
 
-* Every endpoint will be using the users collection, found in `users.json`.
-* Each endpoint in this readme is followed by the path that will be tested. For example, 'Get all users.' will be tested by doing a `GET` request to `/api/users`.
+* Every endpoint will be using the accounts collection, found in `accounts.json`.
+* Each endpoint in this readme is followed by the path that will be tested. For example, 'Get all accounts.' will be tested by doing a `GET` request to `/api/accounts`.
 * You can test your work by running `npm test` in your CLI.
-* When you are finished, you should have `11 tests passed`.
-* Be sure to use the correct method (GET, POST, PUT, or DELETE) and the correct path (i.e. `/api/users`).
+* When you are finished, you should have `9 tests passed`.
+* Be sure to use the correct method (GET, POST, PUT, or DELETE) and the correct path (i.e. `/api/accounts`).
 * All responses must return a status of 200, unless otherwise specified
 
 ##### Make the following endpoints.
 
-1.  Get all users. Return all users from the users array.
-   `GET: /api/users`
+1.  Get all accounts. Return all accounts from the accounts array.
+   `GET: /api/accounts`
    
-2.  Get all users by language. You will receive the `language` as a query ('english, french, spanish or klingon.').  Return an array of all users that have the same language specified in the query.
-   `GET: /api/users?language=klingon`
+2.  Get all accounts by `card_type`. You will receive the `card_type` as a query (visa, mastercard, etc).  Return an array of all accounts that have the same type specified in the query.
+   `GET: /api/accounts?cardtype=visa`
 
-3.  Get all users, filtering by privilege. You will receive the privilege as a url parameter. ('admin, moderator, user').  Return an array of all users with the same privilege specified in the parameter.
-   `GET: /api/users/admin`
+3. Find one account by id. Use the url params to get the id. Make sure that a get request for a account that does not exists returns 404.
+    `GET: /api/accounts/` + accountId
 
-4. Find one user by id. Use the url params to get the id. Make sure that a get request for a user that does not exists returns 404.
-    `GET: /api/users/` + userId
+4. Create a new account.  `POST: /api/accounts`
 
-5. Create a new user.  You will receive the data on the body.  All users will need an id property.   Manage the value so that it increments each time.  Collections should be initialized with a defualt array.  Return a valid status code and the new user object you created (with the id on it).  
-   `POST: /api/users`
+* You will receive the data on the body.  
+* All accounts will need an id property, but it will not be provided to you.  Manage a value so that it increments each time.  
+* Collections should be initialized with a defualt array.  
+* Return a valid status code and the new account object you created (with the id on it).  
 
-6. Create a new admin user. You will receive their privilege as a url parameter. ('admin, moderator, or user').  Return a valid status code.  Make sure to set the privilege correctly.
-   `POST: /api/users/admin`
+6.  Change a account's card type. The language will be sent in the body `{cardtype: "New card type"}`. Update the account in your array and then return a valid status code and the updated account object.
+   `POST: /api/accounts/cardtype/` + accountId
 
-7.  Change a user's language. The language will be sent in the body `{language: "New language"}`. Update the user in your array and then return a valid status code and the updated user object.
-   `POST: /api/users/language` + userId
+7.  Add to a accounts approved states. Use params to get a account by id. You will receive the data on the body `{add: 'New state'}`. Return a valid status code.
+   `POST: /api/accounts/:accountId/approvedstates/` 
 
-8.  Add to a users favorite forums. Use params to get a user by id. You will receive the data on the body `{add: 'New Forum'}`. Return a valid status code.
-   `POST: /api/users/forums/` + userId
+8. Remove from a accounts approved states. Use delete REST method. You will receive the account id in the url params. You will receive the forum to delete in the query. You will need to search your array and splice it out.
+   `DELETE: /api/accounts/:accountID/approvedstates/` + `?state=` + statename.
 
-9. Remove from a users favorite forums. Use delete REST method. You will receive the user id in the url params. You will receive the forum to delete in the query. You will need to search your array and splice it out.
-   `DELETE: /api/users/forums/` + userID + `?favorite=` + forum name.
+9.  Ban (delete) a account. Use REST method delete and the query params to delete a account by their id number.
+   `DELETE: /api/accounts/` + accountID
 
-10.  Ban (delete) a user. Use REST method delete and the query params to delete a user by their id number.
-   `DELETE: /api/users/` + userID
+10. Change your get all accounts (#1) endpoint so that it uses queries. Allow queries for firstname, lastname, balance, cardtype.
 
-11. Change your get all users (#1) endpoint so that it uses queries. Allow queries for age, language, city, state, and gender.
-
-
-12. Update one user by id.  (You'll need to find your user then use a for in loop to update properties).
-   `PUT: /api/users/` + userId
+11. Update one account by id.  (You'll need to find your account then use a for in loop to update properties).
+   `PUT: /api/accounts/` + accountId
--- test.js
+++ test.js
@@ -2,148 +2,125 @@ import test from 'ava';
 import request from 'supertest-as-promised';
 import app from './server';
 import Faker from 'faker';
-import users from './users.json';
+import accounts from './accounts.json';
 
-test('Get all users', async t=> {
+test('Get all accounts', async t=> {
   t.plan(3)
   const res = await request(app)
-    .get('/api/users')
+    .get('/api/accounts')
     .expect(200)
 
   t.is(res.status, 200, 'Status is not 200');
-  t.deepEqual(res.body[0], users[0], "No user returned");
+  t.deepEqual(res.body[0], accounts[0], "No account returned");
   t.truthy(res.body, 'Something is wrong with the response body');
 })
 
-test('Get user by language', async t => {
+test('Get account by card type', async t => {
   let req = await request(app)
-    .get('/api/users?language=klingon')
+    .get('/api/accounts?cardtype=jcb')
     .expect(200)
 
   t.truthy(req.body, 'Request body not found');
 
-  req.body.forEach(function(user) {
-    t.is(user.language, 'klingon')
+  req.body.forEach(function(account) {
+    t.is(account.card_type, 'jcb')
   })
 
   req = await request(app)
-    .get('/api/users?language=french')
+    .get('/api/accounts?cardtype=visa')
     .expect(200)
 
   t.truthy(req.body);
 
-  req.body.forEach(user => {
-    t.is(user.language, 'french');
+  req.body.forEach(account => {
+    t.is(account.card_type, 'visa');
   })
 
   req = await request(app)
-    .get('/api/users?language=foo')
+    .get('/api/accounts?cardtype=foo')
 
   t.is(req.status, 200);
   t.falsy(req.body[0])
 })
 
-test('Get user by privilege', async t => {
+test('Create new account', async t => {
+  let colLength = accounts.length;
+  let newaccount = fakeaccount();
   const req = await request(app)
-    .get('/api/users/admin')
-
-  t.plan(3+req.body.length);
-  t.is(req.status, 200);
-  t.truthy(req.body, 'Something is wrong with the request body');
-  t.truthy(req.body[0], 'No results returned');
-
-  req.body.forEach(user => {
-    t.is(user.type, 'admin')
-  });
-})
-
-test('Create new user', async t => {
-  let colLength = users.length;
-  let newUser = fakeUser();
-  const req = await request(app)
-    .post('/api/users')
-    .send(newUser)
+    .post('/api/accounts')
+    .send(newaccount)
 
   t.is(req.status, 200, 'Make sure you are using bodyParser');
 })
 
-test('Create a new admin user', async t => {
-  let newUser = fakeUser();
-  const req = await request(app)
-    .post('/api/users/admin')
-    .send(newUser)
-
-  t.is(req.status, 200)
-})
-
-test('Change a users language', async t => {
-  let newUser = fakeUser();
+test('Change a accounts card type', async t => {
+  let newaccount = fakeaccount();
   let id;
   let req = await request(app)
-    .post('/api/users')
-    .send(newUser)
+    .post('/api/accounts')
+    .send(newaccount)
 
   t.is(req.status, 200);
   t.truthy(req.body.id);
   id = req.body.id;
 
   req = await request(app)
-    .post('/api/users/language/' + id)
-    .send({language: 'french'})
+    .post('/api/accounts/cardtype/' + id)
+    .send({cardtype: 'bluecard'})
 
-  t.is(req.status, 200, 'error changing language');
+  t.is(req.status, 200, 'error changing cardtype to bluecard');
 
   req = await request(app)
-    .get('/api/users/' + id)
+    .get('/api/accounts/' + id)
 
   t.is(req.status, 200);
-  t.truthy(req.body.language);
-  t.is(req.body.language, 'french');
+  t.truthy(req.body.cardtype);
+  t.is(req.body.cardtype, 'bluecard');
 
   req = await request(app)
-    .post('/api/users/language/' + id)
-    .send({language: 'klingon'})
+    .post('/api/accounts/cardtype/' + id)
+    .send({cardtype: 'greencard'})
 
-  t.is(req.status, 200, 'error changing language to Klingon')
+  t.is(req.status, 200, 'error changing cardtype to greencard')
 
   req = await request(app)
-    .get('/api/users/' + id)
+    .get('/api/accounts/' + id)
 
   t.is(req.status, 200);
-  t.truthy(req.body.language);
-  t.is(req.body.language, 'klingon');
+  t.truthy(req.body.cardtype);
+  t.is(req.body.cardtype, 'greencard');
 })
 
-test('add to a users favorite forums', async t => {
-  let newUser = fakeUser();
+test('add to a accounts approved states', async t => {
+  let newaccount = fakeaccount();
   let id;
 
-  // CREATE A NEW USER
+  // CREATE A NEW account
   let res = await request(app)
-    .post('/api/users')
-    .send(newUser)
+    .post('/api/accounts')
+    .send(newaccount)
 
   t.is(res.status, 200);
   t.truthy(res.body.id);
   id = res.body.id;
 
-  // ADD TO FAVORITE FORUMS
+  // ADD TO approved_states
   res = await request(app)
-    .post('/api/users/forums/' + id)
+    .post('/api/accounts/approvedstates/' + id)
     .send({add: 'foo'})
 
   t.is(res.status, 200);
 
-  // CHECK IF USER HAS NEW FORUM
+  // CHECK IF account HAS NEW FORUM
   res = await request(app)
-    .get('/api/users/' + id)
+    .get('/api/accounts/' + id)
 
   t.is(res.status, 200);
-  t.is(true, Array.isArray(res.body.favorites))
+  t.is(true, Array.isArray(res.body.approved_states))
 
   t.is(
     true,
-    res.body.favorites.reduce((a, b) => {
+    res.body.approved_states.reduce((a, b) => {
       if (b === 'foo') return true;
       else return false;
     }, false)
@@ -151,143 +128,121 @@ test('add to a users favorite forums', async t => {
 
 })
 
-test('remove from a users favorite forums', async t => {
-  let newUser = fakeUser();
+test('remove from a accounts approved_states', async t => {
+  let newaccount = fakeaccount();
   let id;
 
   // CREATE NEW FORUM
   let res = await request(app)
-    .post('/api/users')
+    .post('/api/accounts')
 
   t.is(res.status, 200);
   t.truthy(res.body.id);
   id = res.body.id;
 
-  // ADD TO USER FORUMS
+  // ADD TO account approved_states
   res = await request(app)
-    .post('/api/users/forums/' + id)
+    .post('/api/accounts/approvedstates/' + id)
     .send('foo')
 
   t.is(res.status, 200);
 
-  // Remove from user forums
+  // Remove from account approved_states
   res = await request(app)
-    .delete('/api/users/forums/' + id + '?favorite=foo')
+    .delete('/api/accounts/approvedstates/' + id + '?state=foo')
 
   t.is(res.status, 200);
 
   // Check that forum has been deleted
   res = await request(app)
-    .get('/api/users/' + id);
+    .get('/api/accounts/' + id);
 
   t.is(res.status, 200);
-  t.true(Array.isArray(res.body.favorites));
+  t.true(Array.isArray(res.body.approved_states));
 
-  res.body.favorites.reduce((a,b) => {
+  res.body.approved_states.reduce((a,b) => {
     return b !== 'foo';
   }, false)
 })
 
-test('ban(delete) a user', async t => {
-  let user = fakeUser();
+test('ban(delete) a account', async t => {
+  let account = fakeaccount();
 
-  // Create a new user
+  // Create a new account
   let res = await request(app)
-    .post('/api/users')
-    .send(user)
+    .post('/api/accounts')
+    .send(account)
 
   t.is(res.status, 200);
   t.truthy(res.body.id);
-  user.id = res.body.id;
+  account.id = res.body.id;
 
-  // Delete that user
+  // Delete that account
   res = await request(app)
-    .delete('/api/users/' + user.id)
+    .delete('/api/accounts/' + account.id)
 
   t.is(res.status, 200);
 
-  // Make sure user does not exist
+  // Make sure account does not exist
   res = await request(app)
-    .get('/api/users/' + user.id)
+    .get('/api/accounts/' + account.id)
 
-  t.is(res.status, 404, 'Make sure that when a user is not found that the server returns 404');
+  t.is(res.status, 404, 'Make sure that when a account is not found that the server returns 404');
   t.truthy(res.body);
-  t.falsy(res.body[0], 'User still exists, delete endpoint is not working');
+  t.falsy(res.body[0], 'account still exists, delete endpoint is not working');
 
 })
 
-test('Use queries for get all users', async t => {
-  // age
+test('Use queries for get all accounts', async t => {
+  // first name
   let res = await request(app)
-    .get('/api/users?age=49')
+    .get('/api/accounts?firstname=Jack')
 
   t.is(res.status, 200);
   t.truthy(res.body);
-  res.body.forEach(user => {
-    t.true(user.age == 49, 'Age query failed')
+  res.body.forEach(account => {
+    t.true(account.first_name == "Jack", 'first name query failed')
   })
 
-  // Location
+  // last name
   res = await request(app)
-    .get('/api/users?language=english')
+    .get('/api/accounts?lastname=Morgan')
 
   t.is(res.status, 200);
   t.truthy(res.body);
-  t.truthy(res.body[0], 'Nothing returned from language query');
-  res.body.forEach(user => {
-    t.true(user.language === 'english', 'Language query failed');
+  t.truthy(res.body[0], 'Nothing returned from last name query');
+  res.body.forEach(account => {
+    t.true(account.last_name === 'Morgan', 'last name query failed');
   })
 
-  // City
+  // balance
   res = await request(app)
-    .get('/api/users?city=scranton')
+    .get('/api/accounts?balance=978.52')
 
   t.is(res.status, 200);
   t.truthy(res.body);
-  t.truthy(res.body[0], 'Nothing returned from city query');
+  t.truthy(res.body[0], 'Nothing returned from balance query');
 
-  res.body.forEach(user => {
-    t.true(user.city == 'Scranton', 'City query failed')
+  res.body.forEach(account => {
+    t.true(account.balance == '978.52', 'balance query failed')
   })
 
-  // State
-  res = await request(app)
-    .get('/api/users?state=pennsylvania')
-
-  t.is(res.status, 200);
-  t.truthy(res.body);
-  t.truthy(res.body[0], 'Nothing returned from state query');
-
-  res.body.forEach(user => {
-    t.true(user.state == 'Pennsylvania', 'State query failed')
-  })
-
-  // Gender
-  res = await request(app)
-    .get('/api/users?gender=female')
-
-  t.is(res.status, 200);
-  t.truthy(res.body);
-  t.truthy(res.body[0], 'Nothing returned from gender query');
-
-  res.body.forEach(user => {
-    t.true(user.gender == 'Female');
-  })
+  
 })
 
-test('Update one user', async t => {
-  let user = fakeUser();
+test('Update one account', async t => {
+  let account = fakeaccount();
 
   let res = await request(app)
-    .post('/api/users')
+    .post('/api/accounts')
 
   t.is(res.status, 200);
   t.truthy(res.body.id);
-  user.id = res.body.id;
+  account.id = res.body.id;
 
-  // Update user
+  // Update account
   res = await request(app)
-    .put('/api/users/' + user.id)
+    .put('/api/accounts/' + account.id)
     .send({
       first_name: 'foo',
       last_name: 'bar'
@@ -297,12 +252,12 @@ test('Update one user', async t => {
 
   // Check that update was successful
   res = await request(app)
-    .get('/api/users/' + user.id)
+    .get('/api/accounts/' + account.id)
 
   t.is(res.status, 200);
   t.truthy(res.body);
-  t.is(res.body.first_name, 'foo', 'User was not changed');
-  t.is(res.body.last_name, 'bar', 'User was not changed');
+  t.is(res.body.first_name, 'foo', 'account was not changed');
+  t.is(res.body.last_name, 'bar', 'account was not changed');
 
 })
 
@@ -311,23 +266,18 @@ test('Update one user', async t => {
 
 
 
-function fakeUser() {
-  let languages = ['english', 'french', 'spanish', 'klingon'];
-  let types = ['admin', 'moderator', 'user'];
-  let faves = ['cats', 'dogs', 'angular'];
+function fakeaccount() {
+  let cardtypes = ['visa', 'jcb', 'bluecard', 'orangecard'];
+  let states = ['Utah', 'Arizona', 'Texas'];
   function getRand(l) {
     return Math.floor(Math.random() * l);
   }
   return {
     first_name: Faker.name.firstName(),
     last_name: Faker.name.lastName(),
-    email: Faker.internet.email(),
-    gender: Math.random() >= .5 ? "Male" : "Female",
-    language: languages[getRand(languages.length)],
-    age: Math.floor(Math.random() * 70) + 20,
-    city: Faker.address.city(),
-    state: Faker.address.state(),
-    type: types[getRand(types.length)],
-    favorites: faves[getRand(faves.length)]
+    cardtype: cardtypes[getRand(cardtypes.length)],
+    card_number: Math.floor(Math.random() * 70) + 20,
+    balance: Math.floor(Math.random() * 70) + 20,
+    approved_states: states[getRand(faves.length)]
   }
 }
